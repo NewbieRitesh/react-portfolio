a59bb0cc92b9087a8aa6eadfbfa2dfdfa906a4cc
NewbieRitesh <newbieritesh@gmail.com>
2023-05-05 11:54:07 +0530

    updating projects and links

--- src/components/ProjectStur.js
+++ src/components/ProjectStur.js
@@ -9,24 +9,26 @@ export default function ProjectStur(props) {
                     <span> {props.title}</span></div>
                 <div className="git">
                     <label htmlFor="git">
-                        GitHub Link: 
-                    </label>
-                    <span><a className="text-[rgb(255,75,75)]" href={props.gitLink}> {props.gitLink}</a></span></div>
+                        GitHub Link:
+                    </label><br />
+                    {props.gitLink.backend ? <><span className="text-sm"><label> backend:</label><a className="text-blue-800" href={props.gitLink.backend}> {props.gitLink.backend}</a></span><br /></> : ""}
+                    {props.gitLink.frontend ? <><span className="text-sm"><label> frontend:</label><a className="text-blue-800" href={props.gitLink.frontend}> {props.gitLink.frontend}</a></span><br /></> : ""}
+                </div>
                 <div className="web">
                     <label htmlFor="web">
-                        Web Link: 
+                        Web Link:
                     </label>
-                    <span><a className="text-[rgb(255,75,75)]" href={props.webLink} > {props.webLink !== null && props.webLink ? props.webLink : "Not available"}</a></span></div>
+                    <span><a className="text-blue-800" href={props.webLink} > {props.webLink !== null && props.webLink ? props.webLink : "Not available"}</a></span></div>
                 <div className="description">
                     <label htmlFor="description">
-                        Description: 
+                        Description:
                     </label>
                     <span> {props.desc}</span></div>
                 <div className="tech">
                     <label htmlFor="tech">
                         Technology Used:
                     </label>
-                    <span> {props.tech}</span></div>
+                    <span className="text-blue-800"> {props.tech}</span></div>
             </div>
         </>
     )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
             <div className="bg-transperent backdrop-blur-sm text-center py-5">
                 <h1 className="text-center text-white font-bold text-2xl">Resume</h1>
                 <div className="mt-5 flex flex-col items-center">
-                    <a href="https://drive.google.com/file/d/1zZbvi4JgAfgnmYbwZzmouvnG_VC1zl5r/view?usp=share_link" className="w-48 hover:text-gray-300 m-2 p-3 border-white border-2 text-white font-bold rounded-full">Download Resume</a>
+                    <a href="https://drive.google.com/file/d/1V6WPekPhraf7r7-Iz7bp9bk7zQxvMss2/view?usp=share_link" className="w-48 hover:text-gray-300 m-2 p-3 border-white border-2 text-white font-bold rounded-full">Download Resume</a>
                 </div>
             </div>
         </>
